588a3fe6e48e3581cf439749a168e10d47267bff
boiar <kero2000boiar410@gmail.com>
2022-08-27 20:04:49 +0200

    edit in settings seeder

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -32,7 +32,7 @@ class SettingsSeeder extends Seeder
         }
 
 
-        $settingCheck = DB::table('settings')->where('setting_key', 'whatsapp_number')->get();
+        $settingCheck = DB::table('settings')->where('setting_key', 'whatsapp')->get();
         if (!count($settingCheck)) {
             DB::table('settings')->insert(
 
@@ -40,6 +40,34 @@ class SettingsSeeder extends Seeder
                     'setting_name'  => '{"ar":"رقم الواتساب", "en":"whatsapp number"}',
                     'setting_key'   => 'whatsapp',
                     'setting_value' => '999999990',
+                    'created_at'    => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at'    => Carbon::now()->format('Y-m-d H:i:s'),
+                ]
+            );
+        }
+
+        $settingCheck = DB::table('settings')->where('setting_key', 'tax_in_homepage')->get();
+        if (!count($settingCheck)) {
+            DB::table('settings')->insert(
+
+                [
+                    'setting_name'  => '{"ar":"سعر الاعلان في الصفحة الرئيسية", "en":"Ad price on the home page"}',
+                    'setting_key'   => 'tax_in_homepage',
+                    'setting_value' => '20',
+                    'created_at'    => Carbon::now()->format('Y-m-d H:i:s'),
+                    'updated_at'    => Carbon::now()->format('Y-m-d H:i:s'),
+                ]
+            );
+        }
+
+        $settingCheck = DB::table('settings')->where('setting_key', 'tax_in_discover_page')->get();
+        if (!count($settingCheck)) {
+            DB::table('settings')->insert(
+
+                [
+                    'setting_name'  => '{"ar":"tax_in_discover_page", "en":"Ad price on the Discover page"}',
+                    'setting_key'   => 'tax_in_discover_page',
+                    'setting_value' => '15',
                     'created_at'    => Carbon::now()->format('Y-m-d H:i:s'),
                     'updated_at'    => Carbon::now()->format('Y-m-d H:i:s'),
                 ]
