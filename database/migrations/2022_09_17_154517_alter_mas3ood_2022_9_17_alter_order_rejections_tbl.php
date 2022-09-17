a58cfc10a4cad5d5bbde1f22280dab513f0095b5
boiar <kero2000boiar410@gmail.com>
2022-09-17 17:55:19 +0200

    add new migration file

---
+++ database/migrations/2022_09_17_154517_alter_mas3ood_2022_9_17_alter_order_rejections_tbl.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterMas3ood2022917AlterOrderRejectionsTbl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('order_rejections')) {
+
+            DB::statement("
+              ALTER TABLE app_my_time_test2.order_rejections DROP FOREIGN KEY order_rejections_ibfk_2;
+            ");
+
+
+            DB::statement("
+             ALTER TABLE `order_rejections` ADD FOREIGN KEY (`rejection_reason`) 
+             REFERENCES `order_rejections_reasons`(`rejection_reason_id`) 
+             ON DELETE RESTRICT ON UPDATE RESTRICT;
+            ");
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
