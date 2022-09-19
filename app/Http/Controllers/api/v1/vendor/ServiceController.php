967e73b1d1ddc2df754eeb4b6a480e95fe4ce922
boiar <kero2000boiar410@gmail.com>
2022-09-19 23:18:48 +0200

    vendor_api => edit show service

--- app/Http/Controllers/api/v1/vendor/ServiceController.php
+++ app/Http/Controllers/api/v1/vendor/ServiceController.php
@@ -166,7 +166,7 @@ class ServiceController extends Controller
             return ResponsesHelper::returnError('400',__('vendor.This_service_is_not_for_you'));
         }
 
-        $service = Service::getService($service->service_id);
+        $service = Service::getService($service->service_id, 'api');
 
         $service = array_merge(['service_id'=>(int)$service_id], $service);
 
