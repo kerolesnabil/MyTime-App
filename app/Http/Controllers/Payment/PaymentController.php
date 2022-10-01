e57008b071001fb7e193aefcdad5946857865eb6
boiar <kero2000boiar410@gmail.com>
2022-10-01 12:49:20 +0200

    vendor api => edit in deposit and withdrawal and create migration file

--- app/Http/Controllers/Payment/PaymentController.php
+++ app/Http/Controllers/Payment/PaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Payment;
 use App\Helpers\ResponsesHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Deposit;
+use App\Models\FinancialRequests;
 use App\Models\PaymentMethod;
 use App\Models\Withdrawal;
 use Illuminate\Http\Request;
@@ -24,20 +25,22 @@ class PaymentController extends Controller
         $rules= [
             "amount"=>"required|integer",
         ];
+
         $validator = Validator::make($request->all(), $rules);
         if ($validator->fails()) {
             return ResponsesHelper::returnValidationError('400', $validator);
         }
 
-       $payment_id=PaymentMethod::getPaymentByMethodType('cash');
+       $payment_obj = PaymentMethod::getPaymentByMethodType('cash');
 
-        $data=[
-            'user_id'=>Auth::user()->user_id,
-            'payment_id'=>$payment_id['payment_method_id'],
-            'amount'=>$request->get('amount')
+        $data = [
+            'user_id'          => Auth::user()->user_id,
+            'payment_id'       => $payment_obj['payment_method_id'],
+            'amount'           => $request->get('amount'),
+            'transaction_type' => 'deposit'
         ];
 
-        $deposit=Deposit::createDeposit($data);
+        $deposit = FinancialRequests::createFinancialRequest($data);
 
         return ResponsesHelper::returnData($deposit->deposit_id,'200',trans('payment.send_order'));
     }
@@ -46,27 +49,28 @@ class PaymentController extends Controller
     {
 
         if(Auth::user()->user_type!='vendor'){
+
             return ResponsesHelper::returnError('400',trans('vendor.not_vendor'));
         }
         $rules= [
             "amount"=>"required|integer",
         ];
+
         $validator = Validator::make($request->all(), $rules);
         if ($validator->fails()) {
             return ResponsesHelper::returnValidationError('400', $validator);
         }
-        if(Auth::user()->user_wallet<$request->get('amount'))
-        {
-            return  ResponsesHelper::returnSuccessMessage(trans('vendor.lower_amount'),'205');
-        }
 
-        $payment_id=PaymentMethod::getPaymentByMethodType('cash');
-        $data=[
-            'user_id'=>Auth::user()->user_id,
-            'payment_id'=>$payment_id['payment_method_id'],
-            'amount'=>$request->get('amount')
+        $payment_obj = PaymentMethod::getPaymentByMethodType('cash');
+
+        $data = [
+            'user_id'          => Auth::user()->user_id,
+            'payment_id'       => $payment_obj['payment_method_id'],
+            'amount'           => $request->get('amount'),
+            'transaction_type' => 'withdrawal'
         ];
-        $deposit=Withdrawal::createWithdrawal($data);
+
+        $deposit = FinancialRequests::createFinancialRequest($data);
 
         return ResponsesHelper::returnData($deposit->deposit_id,'200',trans('payment.send_order'));
     }
