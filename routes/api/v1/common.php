060ce6e13016557f4581538c46c05250642c7863
boiar <kero2000boiar410@gmail.com>
2022-12-26 00:15:47 +0200

    edit in APIGate

--- routes/api/v1/common.php
+++ routes/api/v1/common.php
@@ -10,7 +10,7 @@ Route::group([
 
     Route::get('/get-menu-pages', [\App\Http\Controllers\api\v1\common\PagesController::class, 'getMenuPages']);
 
-    Route::group(['middleware' => ['auth:api']],function () {
+    Route::group([],function () {
 
         Route::post('/support-message', [\App\Http\Controllers\api\v1\common\SupportMessagesController::class, 'addSupportMessage']);
         Route::post('/charge-wallet', [\App\Http\Controllers\api\v1\payment\PaymentController::class, 'chargeWalletPayment']);
